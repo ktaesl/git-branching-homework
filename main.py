248a45eec46ce534adbfa6ce9dc1a2e9804e746c
Ekaterina <ekaterinasysuevacollege@gmail.com>
2026-06-20 04:05:08 +0300

    feat: update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # пример начального main.py
 print("Student Branching App")
 print("Проект для тренировки веток Git")
+
+from profile import print_profile
+
+print("Student Branching App")
+print_profile()
