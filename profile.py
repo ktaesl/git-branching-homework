589a949b4e79ddf11fa70b5f2ec87f49e83940dc
Ekaterina <ekaterinasysuevacollege@gmail.com>
2026-06-20 03:48:24 +0300

    feat: добавить информацию об авторе

---
+++ profile.py
@@ -0,0 +1,14 @@
+def get_profile():
+    return {
+        "name": "Екатерина Сысуева",
+        "group": "РПО-1",
+        "role": "студент",
+        "skills": ["Git", "VS Code", "Python"]
+    }
+def get_profile():
+    return {
+        "name": "Екатерина Сысуева",
+        "group": "РПО-1",
+        "role": "студент",
+        "skills": ["Git", "VS Code", "Python"]
+    }
