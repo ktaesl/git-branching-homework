5f586f02be7fbbd45028fd55389d5ff457240473
Ekaterina <ekaterinasysuevacollege@gmail.com>
2026-06-20 04:17:24 +0300

    feat: connect subject function to main.py

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ from profile import print_profile
 
 print("Student Branching App")
 print_profile()
+
+SUBJECTS = [
+    "Основы программирования",
+    "Git и системы контроля версий",
+    "Базы данных",
+    "Веб-разработка"
+]
+
+
+def print_subjects():
+    print("\nДисциплины:")
+    for number, subject in enumerate(SUBJECTS, start=1):
+        print(f"{number}. {subject}")
